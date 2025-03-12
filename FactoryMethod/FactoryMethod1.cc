7042fa24e514f35d6ac152dec200b75ad547c0f8
moranxiao <yyyjka@foxmail.com>
2025-03-12 16:42:49 +0800

    工厂方法模式

---
+++ FactoryMethod/FactoryMethod1.cc
@@ -0,0 +1,20 @@
+class Test1{
+
+};
+
+class Test2{
+    
+};
+
+class Application{
+public:
+    void run()
+    {
+        //...
+        Test1* pTest1 = new Test1;
+        //...
+        Test2* pTest2 = new Test2;
+        //....
+    }
+
+};
